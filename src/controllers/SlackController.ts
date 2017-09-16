9d8792ab7bf1962be5b79a70bb2bb1bee504ea63
Fernando Falci <falci@falci.me>
2017-09-16 16:14:47 +0200

    Added suport for /anon help

--- src/controllers/SlackController.ts
+++ src/controllers/SlackController.ts
@@ -13,6 +13,13 @@ export class SlackController {
             return 'I can\'t be anonymous here!';
         }
 
+        if(message.text === 'help') {
+            return 'Hi, I\'m the Anonymous Bot.\n\n' +
+              'Whenever you want to send an anonymous message, go to the channel and type `/anon <message>`\n\n' +
+              'Example:\n' +
+              '/anon Hello!';
+        }
+
         const options: request.Options = {
             url: message.response_url,
             method: 'POST',
