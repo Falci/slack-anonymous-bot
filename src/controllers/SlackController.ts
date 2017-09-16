d7ec6bb0d65d3f56f984a45944671c11a4203d46
Fernando Falci <falci@falci.me>
2017-09-17 01:50:37 +0200

    Fix for char encode

--- src/controllers/SlackController.ts
+++ src/controllers/SlackController.ts
@@ -1,5 +1,6 @@
 import {Controller, Body, Post} from 'routing-controllers';
-import * as request from 'request';
+import * as http from 'http';
+import * as url from 'url';
 import {IncomingMessage} from '../models/incoming.model';
 import {OutcomingMessage} from '../models/outcoming.model';
 
@@ -19,16 +20,21 @@ export class SlackController {
               '/anon Hello!';
         }
 
-        const options: request.Options = {
+        const u = message.text.startsWith('debug') ? 'https://requestb.in/18x7gql1' : message.response_url
+
+        const options = Object.assign(url(u), {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
-            },
-            form: OutcomingMessage.inChannel(message.text).json()
-        };
+            }
+        });
 
         return new Promise<string>((resolve) => {
-            request.post('https://requestb.in/1l6e2yx1' /*message.response_url*/, options, (a, b, c) => console.log(c) || resolve(''));
+
+            const request = http.request(options, (res) => res.on('end', () => resolve('')));
+            request.write(OutcomingMessage.inChannel(message.text).json());
+            request.end();
+
         });
     }
 
